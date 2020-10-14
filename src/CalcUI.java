e8f5d9be2325b870fbc043f328f95689aa2abb4d
DhruvK0 <46884021+DhruvK0@users.noreply.github.com>
2020-10-14 08:56:20 -0700

    Added Grid Layout for Buttons

--- src/CalcUI.java
+++ src/CalcUI.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+
 public class CalcUI {
+    private JPanel panel1;
+    private JButton a0Button;
+    private JButton button2;
+    private JButton a2Button;
+    private JButton a1Button;
+    private JButton a5Button;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a7Button;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton button12;
+    private JButton button1;
+    private JButton button3;
+    private JButton xButton;
+    private JButton button5;
 }
