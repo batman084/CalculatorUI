af42b78d58c73f072472c3d7d20c1367163ca402
batman084 <awesomegautam1@gmai.com>
2020-10-14 09:03:12 -0700

    added grid/text

--- src/CalcUI.java
+++ src/CalcUI.java
@@ -11,7 +11,7 @@ public class CalcUI {
     private JButton a8Button;
     private JButton a7Button;
     private JButton a9Button;
-<<<<<<< HEAD
+
     private JButton a6Button;
     private JButton a3Button;
     private JButton button12;
@@ -19,34 +19,13 @@ public class CalcUI {
     private JButton button3;
     private JButton xButton;
     private JButton button5;
-=======
-    private JButton Num3;
-    private JButton Equal;
-    private JButton Add;
-    private JButton Multiply;
-    private JButton Divide;
-    private JTextArea Text;
-    private JButton Num4;
-    private JButton Num5;
-    private JButton Num6;
-    private JButton Subtract;
 
+    private JTextArea bruhTextArea;
 
-    public CalcUI() {
-        Num7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Test");
-            }
-        });
-        a8Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 
+
     //bruh
->>>>>>> a7702dc... test
 }
